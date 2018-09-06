448294242b13c8e01c9bc4dc8a39e6b99efa70a4
bobbyxbanks <b_x_m@live.com>
2018-09-05 20:30:05 -0700

    updated files and characters

--- clickygame/src/components/Nav/Nav.js
+++ clickygame/src/components/Nav/Nav.js
@@ -1,18 +1,21 @@
-// tried to get page to load current game score and also highscore into html
-
 import React from "react";
+import NavMessage from "../NavMessage";
 import "./Nav.css";
 
+// Component for the Navbar
+
 const Nav = props => (
-    <div>
-        <ul>
-            <li><a>Clicky Game</a></li>
-            <li>
-                {props.message}
-            </li>
-            <li>Score: <span>{props.gameScore}</span> | Top Score: {props.highScore}</li>
-        </ul>
-    </div>
+  <nav className="navbar">
+    <ul>
+      <li className="brand">
+        <a href="/">Clicky Game</a>
+      </li>
+      <NavMessage score={props.score} topScore={props.topScore} />
+      <li>
+        Score: {props.score} | Top Score: {props.topScore}
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Nav;
